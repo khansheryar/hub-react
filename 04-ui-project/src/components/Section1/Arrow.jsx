d14ade1f4fef025d1806f0ef174d07561b9d5825
khansheryar <150885376+khansheryar@users.noreply.github.com>
2026-01-17 08:17:26 -0500

    Delete 04-ui-project directory

--- 04-ui-project/src/components/Section1/Arrow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ArrowUpRight } from 'lucide-react';
-const Arrow = () => {
-  return (
-   <div className=' font-bold '><ArrowUpRight size={100}/></div>
-  )
-}
-
-export default Arrow
