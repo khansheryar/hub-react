0888ddc8ab6860d299c0b847bf9d8bd5010d5685
khansheryar <150885376+khansheryar@users.noreply.github.com>
2026-01-17 08:16:34 -0500

    Delete 01-folder directory

--- 01-folder/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card(){
-    return (
-     <div className="card">
-      <h1>hubaib khan</h1>
-      <p>Lorem ipsum dequatu  djkaf dafafdkadjf ic ut distinctio, quos in.</p>
-     </div>
-    )
-}
-export default Card
